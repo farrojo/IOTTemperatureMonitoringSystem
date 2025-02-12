acc518f34c1cec367ed14a32b6bec6d907c87273
farrojo <ofrojasc@gmail.com>
2025-02-12 13:33:33 -0500

    Data structutre defined

---
+++ IoTDashboard/Models/TelemetryData.cs
@@ -0,0 +1,9 @@
+namespace IoTDashboard.Models
+{
+    public class TelemetryData
+    {
+        public string DeviceId { get; set; }
+        public double Temperature { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
