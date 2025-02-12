9c400e8f4f0ed29113d79c6b62dc6ee09b536fb3
farrojo <ofrojasc@gmail.com>
2025-02-12 13:33:13 -0500

    Dashboard upodated

--- IoTDashboard/Program.cs
+++ IoTDashboard/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Dashboard IoT en ejecución...");
+        await EventHubReceiver.ReceiveMessagesAsync();
+    }
+}
