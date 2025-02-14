30b5b7a6c31f53143e9fb0c13543581a6d33e52e
farrojo <ofrojasc@gmail.com>
2025-02-14 12:07:55 -0500

    sensor.h updated

--- ESP32-Firmware/src/sensor.h
+++ ESP32-Firmware/src/sensor.h
@@ -1,10 +1,12 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+#include <Arduino.h>
 #include <DHT.h>
 
-#define DHTPIN 4
-#define DHTTYPE DHT22
+// Definir el pin del sensor y el tipo de sensor
+#define DHTPIN 4         
+#define DHTTYPE DHT22    // DHT11 o DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 
