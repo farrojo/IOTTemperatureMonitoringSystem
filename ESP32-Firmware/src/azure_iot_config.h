c1e7bb27fb04bf81f45056fcda5fa492815bc41b
farrojo <ofrojasc@gmail.com>
2025-02-14 11:52:32 -0500

    Azure configuration created

---
+++ ESP32-Firmware/src/azure_iot_config.h
@@ -0,0 +1,8 @@
+#ifndef AZURE_IOT_CONFIG_H
+#define AZURE_IOT_CONFIG_H
+
+#define MQTT_SERVER "TU_IOT_HUB.azure-devices.net"
+#define DEVICE_ID "SensorTemp01"
+#define MQTT_TOPIC "devices/SensorTemp01/messages/events/"
+
+#endif
